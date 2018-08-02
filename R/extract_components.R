ffc371401b3fef94479e5db5844a1e20c67ecb86
cimentadaj <cimentadaj@gmail.com>
2018-08-02 17:54:17 +0200

    Polish extract_metadata and add documentation for all extract_ functions

--- R/extract_components.R
+++ R/extract_components.R
@@ -1,7 +1,80 @@
 
-## Checks data_list is correct
-## As we find further requiremenrs that a data_list
-## must have, we add it here
+## Two examples to test the functions below
+
+id <- 'l01080193-numero-total-de-edificios-con-viviendas-segun-numero-de-plantas'
+resp <- httr::content(get_resp(path_dataset_id(id)))
+data_list <- resp$result$items[[1]]
+
+# resp <- httr::content(get_resp(path_datasets()))
+# data_list <- resp$result$items[[2]]
+
+#' Extract all metadata from a data_list
+#'
+#' @param data_list A data_list similar to resp$result$items[[1]] that contains information on a dataset
+extract_metadata <- function(data_list) {
+
+  keywords <- extract_keywords(data_list)
+  title <- extract_title(data_list)
+  description <- extract_description(data_list)
+  languages <- extract_language(data_list)
+  url_path <- extract_url(data_list)
+  url_formats <- extract_url_format(data_list)
+  date_data <- extract_date(data_list)
+  publisher <- extract_publisher(data_list)
+
+
+  # Title, description, languages and url_path
+  # are vector of the same length as the number of
+  # languages. We construct them with N rows
+  # and then add the url_path (which is
+  # the path to the info of the dataset
+  # and no the accessURL). That is, the data frame
+  # is in long format
+
+  first_df <-
+    dplyr::tibble(
+      language = languages,
+      title = title,
+      description = description,
+    )
+
+  first_df$url <- url_path
+
+  # Do not change this to tibble because
+  # all_dfs is a df and it will throw an error.
+  # data.frame in this case allows to pass a data frrame
+  # and bins the columns together
+  data_df <-
+    data.frame(
+      keywords = keywords,
+      first_df,
+      date = date_data,
+      publisher = publisher,
+      stringsAsFactors = FALSE
+    )
+
+  final_df <- dplyr::as_tibble(data_df)
+  final_df
+}
+
+
+# Some of the extract_components return vectors of length > 1
+# that refers to many languages. Like the description
+# comes in three langauges. We want those vectors to be turned
+# into columns with the language prefix in the column names
+vector_to_df_columnwise <- function(vec, column_names) {
+  semi_df <- dplyr::as_tibble(matrix(vec, ncol = length(vec)))
+  names(semi_df) <- column_names
+  semi_df
+}
+
+#' Check data_list is in correct formats
+#'
+#' When new checks come up, add them in
+#' the same format: logical tests first
+#' and then add them to the if statement
+#'
+#' @inheritParams data_list_correct
 data_list_correct <- function(data_list) {
   wrong_length <- length(data_list) == 0
   no_names <- is.null(attr(data_list, 'names'))
@@ -12,40 +85,16 @@ data_list_correct <- function(data_list) {
   no_description_slot <- !'description' %in% names(data_list)
 
 
-  if (wrong_length | no_names | no_distribution_slot) {
+  if (wrong_length | no_names | no_distribution_slot | no_description_slot) {
     return(FALSE)
   }
 
   TRUE
 }
 
-
-### Here's a set of functions to extract elements from
-### a slot with data information
-
-
-## Extract elements from that data_list
-## For now I think we should concentrate on
-# Keywords
-# Title
-# Description
-# Access URL
-# Language
-# Date
-# Publisher
-
-
-## Two examples to test the functions below
-
-id <- 'l01080193-numero-total-de-edificios-con-viviendas-segun-numero-de-plantas'
-resp <- content(get_resp(path_dataset_id(id)))
-data_list <- resp$result$items[[1]]
-
-# resp <- content(get_resp(path_datasets()))
-# data_list <- resp$result$items[[1]]
-
-##
-
+#' Extract keywords from data_list
+#'
+#' @inheritParams data_list_correct
 extract_keywords <- function(data_list) {
 
   if (!data_list_correct(data_list)) {
@@ -60,6 +109,9 @@ extract_keywords <- function(data_list) {
   keywords
 }
 
+#' Extract title from data_list
+#'
+#' @inheritParams data_list_correct
 extract_title <- function(data_list) {
 
   if (!data_list_correct(data_list)) {
@@ -74,6 +126,9 @@ extract_title <- function(data_list) {
   title
 }
 
+#' Extract description from data_list
+#'
+#' @inheritParams data_list_correct
 extract_description <- function(data_list) {
   if (!data_list_correct(data_list)) {
     return(character())
@@ -88,7 +143,27 @@ extract_description <- function(data_list) {
   descriptions
 }
 
+#' Extract URL from datos.gob.es from data_list
+#'
+#' @inheritParams data_list_correct
 extract_url <- function(data_list) {
+
+  if (!data_list_correct(data_list)) {
+    return(character())
+  }
+
+  if (!'_about' %in% names(data_list)) {
+    "No URL available"
+  }
+
+  info_url <- data_list$`_about`
+  info_url
+}
+
+#' Extract access URL to the actual data from data_list
+#'
+#' @inheritParams data_list_correct
+extract_access_url <- function(data_list) {
   if (!data_list_correct(data_list)) {
     return(character())
   }
@@ -102,11 +177,18 @@ extract_url <- function(data_list) {
   access_url
 }
 
+#' Extract the format of the dataset from data_list
+#'
+#' For example, csv or xml
+#' @inheritParams data_list_correct
 extract_url_format <- function(data_list) {
-  urls <- extract_url(data_list)
+  urls <- extract_access_url(data_list)
   sub('.*\\.', '', urls)
 }
 
+#' Extract access languages available from data_list
+#'
+#' @inheritParams data_list_correct
 extract_language <- function(data_list) {
   if (!data_list_correct(data_list)) {
     return(character())
@@ -121,6 +203,11 @@ extract_language <- function(data_list) {
   languages
 }
 
+#' Extract the date at which the data was submitted from data_list
+#'
+#' The date is currently exported as a string but
+#' should be turned into a a date class
+#' @inheritParams data_list_correct
 extract_date <- function(data_list) {
   if (!data_list_correct(data_list)) {
     return(character())
@@ -135,11 +222,14 @@ extract_date <- function(data_list) {
   data_list$issued
 }
 
-# This is wrong on purpose. Still waiting
-# to create the function to extract
-# the publishers automatically
-# to call this and match the publisher ID
-# to the actual name.
+#' Extract the publisher of the dataset from data_list
+#'
+#' This is wrong on purpose. Still waiting
+#' to create the function to extract
+#' the publishers automatically
+#' to call this and match the publisher ID
+#' to the actual name.
+#' @inheritParams data_list_correct
 extract_publisher <- function(data_list) {
   if (!data_list_correct(data_list)) {
     return(character())
@@ -151,70 +241,4 @@ extract_publisher <- function(data_list) {
 
   publisher <- data_list$publisher
   publisher
-}
-
-
-# Some of the extract_components return vectors of length > 1
-# that refers to many languages. Like the description
-# comes in three langauges. We want those vectors to be turned
-# into columns with the language prefix in the column names
-vector_to_df_columnwise <- function(vec, column_names) {
-  semi_df <- dplyr::as_tibble(matrix(vec, ncol = length(vec)))
-  names(semi_df) <- column_names
-  semi_df
-}
-
-
-# If half_columns, returns only the necessary columns
-build_df_datalist <- function(data_list, half_columns = TRUE) {
-
-  keywords <- extract_keywords(data_list)
-  title <- extract_title(data_list)
-  description <- extract_description(data_list)
-  languages <- extract_language(data_list)
-  url_path <- extract_url(data_list)
-  url_formats <- extract_url_format(data_list)
-  date_data <- extract_date(data_list)
-  publisher <- extract_publisher(data_list)
-
-
-  first_column_names <-
-    lapply(c("title_", "description_", "language_"),
-           function(x) paste0(x, languages))
-
-  second_column_names <- list(paste0("url_", url_formats))
-
-  # Title, description, languages and url_path
-  # are vector of the same length as the number of
-  # languages. We want thosse vectors to be
-  # data frames with as many columns as there are languages
-  # the below expression loops through each of the
-  # data_list slot, turns it into a df and creates
-  # column names based on the specific language
-  all_dfs <-
-    dplyr::bind_cols(
-      Map(vector_to_df_columnwise,
-      list(title, description, languages, url_path),
-      c(first_column_names, second_column_names)
-      )
-    )
-
-  # Do not change this to tibble because
-  # all_dfs is a df and it will throw an error.
-  # data.frame in this case allows to pass a data frrame
-  # and bins the columns together
-  data_df <-
-    data.frame(
-      keywords = keywords,
-      all_dfs,
-      date = date_data,
-      publisher = publisher
-    )
-
-  if (half_columns) {
-    data_df <- data_df[grepl("description|url|publisher", names(data_df))]
-  }
-
-  final_df <- dplyr::as_tibble(data_df)
-  final_df
 }
