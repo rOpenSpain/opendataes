2eb38e78c8b753646113df8cc18c8fa5be3e4abe
cimentadaj <cimentadaj@gmail.com>
2018-08-02 17:54:40 +0200

    Add documentation for all url_paths

--- R/url_paths.R
+++ R/url_paths.R
@@ -1,13 +1,9 @@
 #' Build a custom url using the httr url class
 #'
-#' @param query_path the end path of the dataset of interest
+#' @param path the end path of the dataset of interest
 #' @param param arguments for a query
 #' @param ... any other arguments to building the path correctly. See \code{modify_url}
 #'
-#' @return
-#' @export
-#'
-#' @examples
 make_url <- function(path, param, ...) {
   hostname <- "datos.gob.es/apidata"
 
@@ -27,38 +23,54 @@ make_url <- function(path, param, ...) {
       class = "url"
     )
 
-  build_url(semi_url)
+  httr::build_url(semi_url)
 }
 
-# LEt's develop a 'factory' of path functions that will
-# return all different paths but making everything modular.
-# Only one function is in charge of one path.
+
+#' Build a url with a complete catalog/ prefix URL
+#'
+#' @inheritParams make_url
 path_catalog <- function(path, param = NULL, ...) {
   make_url(paste0("catalog/", path), param = param, ...)
 }
 
-## these are all 'static' urls, meaning that they have
-# no parameters to add.
 
+#' Build a url with a complete datasets/ prefix URL
+#'
+#' @param param Extra parameters to add to the url. For this function, this is
+#' useless because there's not further paths to the dataset end point. Keeping
+#' the argument for consistency
+#' @param ... Extra arguments passed to \code{\link[httr]{build_url}}
 path_datasets <- function(param = NULL, ...) {
   path_catalog("dataset", param, ...)
 }
 
+#' Build a url with a complete publishers/ prefix URL
+#'
+#' @param param Extra parameters to add to the url. For this function, this is
+#' useless because there's not further paths to the publishers end point. Keeping
+#' the argument for consistency
+#' @param ... Extra arguments passed to \code{\link[httr]{build_url}}
 path_publishers <- function(param = NULL, ...) {
   path_catalog("publisher", param, ...)
 }
 
+#' Build a url with a complete distribution/ prefix URL
+#'
+#' @param param Extra parameters to add to the url. For this function, this is
+#' useless because there's not further paths to the distribution end point. Keeping
+#' the argument for consistency
+#' @param ... Extra arguments passed to \code{\link[httr]{build_url}}
 path_distribution <- function(param = NULL, ...) {
   path_catalog("distribution", param, ...)
 }
 
 
-
-# Therse are 'dynamic' urls meaning that they have additional
-# parameters that need to be specified to get the data
-
-# ID here is the id of the dataset
-# like 'l01080193-numero-total-de-edificios-con-viviendas-segun-numero-de-plantas'
+#' Build a url with an ID of a dataset
+#'
+#' @param id dataset id from datos.gob.es such as 'l01080193-numero-total-de-edificios-con-viviendas-segun-numero-de-plantas'
+#' @param param Extra parameters to add to the url.
+#' @param ... Extra arguments passed to \code{\link[httr]{build_url}}
 path_dataset_id <- function(id, param = NULL, ...) {
   httr::modify_url(paste0(path_datasets(), "/", id), query = param, ...)
 }
