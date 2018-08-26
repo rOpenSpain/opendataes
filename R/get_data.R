040ad1babb7262c3f5fe85dbf81c605093aef8cd
jlopezper <jorge.loper@gmail.com>
2018-08-26 09:09:47 +0100

    update get_data function with while loop

--- R/get_data.R
+++ R/get_data.R
@@ -25,42 +25,39 @@ get_data <- function(data_list) {
   # Check if the data_list is readable
   is_file_readable <- is_readable(data_list)
 
-  # If readable, get data
-  # If not, get tibble with URL
-  if (length(is_file_readable) != 0) {
+  # Initialize while-loop
+  output_data <- dplyr::tibble()
+  class(output_data) <- "try-error"
 
-    # Get the first format.
-    format_to_read <- is_file_readable[1]
-    args_rio <-
-      list(file = names(format_to_read),
-           format = format_to_read,
-           setclass = "tibble")
+  while(is(output_data, "try-error")) {
 
-    # The encoding argument is only available for the formats below, so
-    # it should be added only when that format is the one being read.
-    # I use is_file_readable and the data forms without the . because
-    # it's easier to match than data_format which has two slashes
-    if (format_to_read %in% c("csv", "html", "xml")) {
-      args_rio <- c(args_rio, "encoding" = "UTF-8")
+    if(length(is_file_readable) != 0) {
+      # Get the first format.
+      format_to_read <- is_file_readable[1]
+      args_rio <-
+        list(file = names(format_to_read),
+             format = format_to_read,
+             setclass = "tibble")
+
+      # The encoding argument is only available for the formats below, so
+      # it should be added only when that format is the one being read.
+      # I use is_file_readable and the data forms without the . because
+      # it's easier to match than data_format which has two slashes
+      if (format_to_read %in% c("csv", "html", "xml")) {
+        args_rio <- c(args_rio, "encoding" = "UTF-8")
+      }
       # This encoding does not work perfectly. Sometimes
       # it does't work and UTF-8 captures spanish accents but
       # other times it doesn't.
-    }
 
-    # Try reading the data
-    output_data <- try(do.call(rio::import, args_rio), silent = TRUE)
-
-    # If there's any error, return an empty tibble
-    if (is(output_data, "try-error")) {
+      # Try reading the data
+      output_data <- try(do.call(rio::import, args_rio), silent = TRUE)
+      is_file_readable <- is_file_readable[-1]
+    } else {
       output_data <- dplyr::tibble(extract_url_format(data_list),
                                    extract_access_url(data_list))
       names(output_data) <- c("format", "URL")
     }
-
-  } else {
-    output_data <- dplyr::tibble(extract_url_format(data_list),
-                                 extract_access_url(data_list))
-    names(output_data) <- c("format", "URL")
   }
 
   # Output
