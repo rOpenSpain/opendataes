cc2fefe80a27373c31f98dc04ce90b1f9a487e08
cimentadaj <cimentadaj@gmail.com>
2018-10-04 22:20:46 +0200

    Adds tests for publishers_available

--- tests/testthat/test-datos_publisher.R
+++ tests/testthat/test-datos_publisher.R
@@ -8,3 +8,12 @@ test_that("datos_publisher returns correct format", {
   expect_gte(nrow(publishers), 50)
   expect_true(all(vapply(publisher, is.character, FUN.VALUE = logical(1))))
 })
+
+
+test_that("publishers_available returns correct format", {
+  publisher <- publishers_available
+
+  expect_is(publisher, "data.frame")
+  expect_equal(ncol(publisher), 2)
+  expect_true(all(vapply(publisher, is.character, FUN.VALUE = logical(1))))
+})
