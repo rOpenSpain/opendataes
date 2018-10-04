f7ef0d49d63ad2fcc6bc09eb826587b582bc329b
cimentadaj <cimentadaj@gmail.com>
2018-10-04 21:58:33 +0200

    Move publisher functions together

--- R/is_fun.R
+++
@@ -1,7 +0,0 @@
-#' Check if publisher is available in opendataes
-#'
-#' @param data_list A data_list similar to resp$result$items[[1]] that contains information on a dataset
-is_publisher_available <- function(data_list) {
-  publisher_code <- tolower(extract_publisher_code(data_list))
-  ifelse(publisher_code %in% tolower(publishers_available$id), TRUE, FALSE)
-}
