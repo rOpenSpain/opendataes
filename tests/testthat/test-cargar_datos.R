63ad7e6cbc65449d50938b6f056310722d2a93f1
Jorge Cimentada <cimentadaj>
2018-10-21 18:51:08 +0200

    Adds tests for cargar_datos

--- tests/testthat/test-cargar_datos.R
+++ tests/testthat/test-cargar_datos.R
@@ -66,11 +66,61 @@ test_that("cargar_datos doesn't read if it's not a character of length 1", {
   expect_error(cargar_datos.character(example_id), "`x` must be a character of length 1")
 })
 
+test_that("cargar_datos uses encoding and readr arguments to read only 5 rows", {
+  tst <-
+    cargar_datos('l01080193-elecciones-al-parlamento-europeo-sobre-electores',
+                 'latin1',
+                 n_max = 5)
+
+  expect_true(all(vapply(tst$data, nrow, FUN.VALUE = numeric(1)) == 5))
+})
+
 
 test_that("cargar_datos returns links when format is not readable", {
   skip_on_cran()
 
   not_readable <- cargar_datos("l01080193-estaciones-de-bicing-mecanicas-y-electricas")
   standard_check(not_readable)
+})
+
+# No need to check for the keyword's format and content because if this passes
+# it means it has the same structure as the character tests
+test_that("cargar_datos's character and keyword results match exactly", {
+  character_method <- cargar_datos('l01080193-elecciones-al-parlamento-europeo-sobre-electores', 'latin1', n_max = 5)
+
+  kw <- explorar_keywords("la Prosperitat", publishers_available$publisher_code)
+
+  intm <- kw[grepl("Elecciones al Parlamento Europeo. % sobre electores", kw$description), ]
+  keyword_method <- cargar_datos(intm, 'latin1', n_max = 5)
+
+  expect_identical(character_method, keyword_method)
+})
+
+test_that("cargar_datos throws errors when the keyword data frame is not in expected formed", {
+  kw <- explorar_keywords("la Prosperitat", publishers_available$publisher_code)
+  cp <- kw
+
+  expect_error(cargar_datos(kw),
+               "The data frame resulted from explorar_keywords must have only 1 dataset (1 row). Make sure you filter down to only one dataset",
+               fixed = TRUE)
+
+  cp <- cp[grepl("Elecciones al Parlamento Europeo. % sobre electores", cp$description), ]
+  cp$path_id <- factor(path_id)
+
+  expect_error(cargar_datos(cp),
+               "The data frame resulted from explorar_keywords must have only 1 dataset (1 row). Make sure you filter down to only one dataset",
+               fixed = TRUE)
+
+
+  expect_identical(character_method, keyword_method)
+})
+
+test_that("cargar_datos for empty df throws error", {
+  kw <- explorar_keywords("la Prosperitat", publishers_available$publisher_code)
+
+  intm <- kw[grepl("Elecciones al Parlamento Europeo. % sobre electores", kw$description), ]
+  keyword_method <- cargar_datos(intm, 'latin1', n_max = 5)
+
+  expect_identical(character_method, keyword_method)
 })
 
