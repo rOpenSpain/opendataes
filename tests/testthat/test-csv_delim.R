5e421907665d033f14b6e8cf8ddbc2cb18525cbc
jlopezper <jorge.loper@gmail.com>
2018-11-24 19:05:04 +0100

    updates new encoding argument in test

--- tests/testthat/test-csv_delim.R
+++ tests/testthat/test-csv_delim.R
@@ -1,25 +1,27 @@
 context("test-csv_delim.R")
 
+encoding <- 'UTF-8'
+
 out1 <- csv_delim("a,b\n1,2,3\n")
 
 test_that("csv_delim can comma separated", {
   # Two rows correctly formatted
-  expect_equal(csv_delim("1,2,3\n4,5,6"), ",")
+  expect_equal(csv_delim("1,2,3\n4,5,6", encoding), ",")
 
   # Can read with empty row
-  expect_equal(csv_delim("a,b\n\n\n1,2"), ",")
+  expect_equal(csv_delim("a,b\n\n\n1,2", encoding), ",")
 
   # Can read many empty rows
-  expect_equal(csv_delim("a,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2"), ",")
+  expect_equal(csv_delim("a,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2", encoding), ",")
 
   # Can read empty rows at beginnin
-  expect_equal(csv_delim("\n\na,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2"), ",")
+  expect_equal(csv_delim("\n\na,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2", encoding), ",")
 
   # Can read empty rows at end
-  expect_equal(csv_delim("a,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2\n"), ",")
+  expect_equal(csv_delim("a,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2\n", encoding), ",")
 
   # Can read empty rows at begin and end
-  expect_equal(csv_delim("\na,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2\n"), ",")
+  expect_equal(csv_delim("\na,b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n1,2\n", encoding), ",")
 })
 
 # csv_delim("v1,v2
