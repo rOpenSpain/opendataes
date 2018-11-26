9a83d78a4247b855bae794207a2f63cffdc60212
Jorge López Pérez <jorge.loper@gmail.com>
2018-11-26 11:32:15 +0100

    VALENCIA TESTED! Adding LPGC

--- R/publishers.R
+++ R/publishers.R
@@ -30,8 +30,9 @@ cargar_publishers <- function() {
 #'
 #' publishers_available
 publishers_available <- tibble::tibble(
-  publishers = c("Ayuntamiento de Barcelona", "Ayuntamiento de Madrid", "Ayuntamiento de Valencia"),
-  publisher_code = c("L01080193", "L01280796", "L01462508")
+  publishers = c("Ayuntamiento de Barcelona", "Ayuntamiento de Madrid", "Ayuntamiento de Valencia", 
+                 "Ayuntamiento de Las Palmas de Gran Canaria"),
+  publisher_code = c("L01080193", "L01280796", "L01462508", "L01350167")
 )
 
 #' Translate publisher code to publisher name
